bc1e7f11f0fe9f7e6c65758e2adabc1d12513678
dgs99001 <dgs99001@proton.me>
2026-03-08 11:17:20 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,10 @@ function App() {
   }, []);
 
   const handleAppClick = (url: string) => {
-    window.open(url, '_blank');
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
+    if (newWindow) {
+      newWindow.opener = null;
+    }
   };
 
   const openTawkChat = () => {
